5e3e56cf2fe28836ed75bd78e712e989384cf19b
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-04-24 17:06:17 -0400

    Fleshing out tank & bullet objects

--- objects.py
+++ objects.py
@@ -1,4 +1,6 @@
 import pygame
+import math
+
 
 # Wii Play Tank Objects
 
@@ -14,6 +16,12 @@ class Tank(pygame.sprite.Sprite):
         self.x = x
         self.y = y
 
+    def loc(self):
+        return self.x, self.y
+
+    def shoot(self, target_coord):
+        Bullet("Assets/bullet16.jpg", self.x, self.y, target_coord[0], target_coord[1])
+
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, image, x, y, target_x, target_y):
@@ -25,13 +33,20 @@ class Bullet(pygame.sprite.Sprite):
         self.y = y
         self.target_x = target_x
         self.target_y = target_y
-        
+        self.displaceVector = self.target_x - self.x, self.target_y - self.y
+        self.displaceVectorMagnitude = abs(math.sqrt(((self.target_x - self.x) ** 2) + ((self.target_y - self.y) ** 2)))
+        self.unitVector = self.displaceVector[0] / self.displaceVectorMagnitude, \
+                          self.displaceVector[1] / self.displaceVectorMagnitude
+
     def loc(self):
         return self.x, self.y
+
+    def update(self):
+        self.x += self.unitVector[0]
+        self.y += self.unitVector[1]
 
 
 class RocketBullet(Bullet):
     def __init__(self, image, x, y, target_x, target_y):
         super().__init__(image, x, y, target_x, target_y)
-
 
