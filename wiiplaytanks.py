b5d4c11fe96ad2229121a0ba82744aacf3db3a92
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-04-25 12:44:28 -0400

    Refactoring

--- wiiplaytanks.py
+++ wiiplaytanks.py
@@ -9,14 +9,14 @@ class SpriteSheet(object):
     def __init__(self, filename):
         self.sheet = pygame.image.load(filename).convert()
 
-    def image_at(self, rectangle, colorkey=None):
+    def image_at(self, rectangle, colorKey=None):
         rect = pygame.Rect(rectangle)
         image = pygame.Surface(rect.size).convert()
         image.blit(self.sheet, (0, 0), rect)
-        if colorkey is not None:
-            if colorkey == -1:
-                colorkey = image.get_at((0, 0))
-            image.set_colorkey((colorkey, pygame.RLEACCEL))
+        if colorKey is not None:
+            if colorKey == -1:
+                colorKey = image.get_at((0, 0))
+            image.set_colorkey((colorKey, pygame.RLEACCEL))
         return image
 
 # Basic Objects
