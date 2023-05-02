d7c486bd7cf3535b2f398aa19a2fcc889c245f27
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-05-02 19:41:19 -0400

    Reworked explosion

--- wiiplaytanks.py
+++ wiiplaytanks.py
@@ -188,23 +188,39 @@ class Bomb(pygame.sprite.Sprite):
 
     def kill(self) -> None:
         super().kill(self)
-        
+ 
 
-               
 class Explosion(pygame.sprite.Sprite):
-    def __init__(self, imageList, coords, explosionGroup, allGroup) -> None:
+
+    def __init__(self, x, y, size) -> None:
         """
-        :param coords: XY Axis 
-        :type coords: tuple
+        :param x: X Axis location
+        :type x: float
+        :param y: Y Axis location
+        :type y: float
         """
-        super().__init__(self)
-        self.imageList = imageList
-        self.x = coords[0]
-        self.y = coords[1]
-        self.counter = 0
-        explosionGroup.add(self)
-        allGroup.add(self)
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.size = size
+        self.images = []
+        self.imagenumber = 0
+        for i in range(1,9):
+            image = pygame.image.load(str(i) + '.png')
+            image = pygame.transform.scale_by(image, size)
+            self.images.append(image)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def update(self) -> None:
-        self.counter += 1
+        if self.imagenumber == 8:
+            self.kill()
+        else:
+            self.image = self.images[self.imagenumber]
+            self.rect = self.image.get_rect()
+            self.rect.x = self.x
+            self.rect.y = self.y
+            self.imagenumber += 1
         
